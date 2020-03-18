8d01104c54c767b02c2c760f3ceb584cc2a2058d
KL <dev.lau3@gmail.com>
2020-03-17 22:36:04 -0500

    modified links between index.html and the game page. Added more images

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#startGameBtn").click(function(){
+        console.log("click detected!!!!!");
+        window.location.replace("../pages/projectileGameInProgress.html");
+    });
+    $("#onImgLink").click(function(){
+        console.log("click detected!!!!!");
+        window.location.replace("../pages/projectileGameInProgress.html");
+    });
+  });
